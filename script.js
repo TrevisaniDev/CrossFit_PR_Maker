f3c2dda0b8b0797f8881e85bf329ef6e9ff4028c
Leonardo Trevisani <leo.hipolito64@hotmail.com>
2025-02-12 20:48:34 -0300

    Firts Functions
    Add some functions to project

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
+document.addEventListener("DOMContentLoaded", loadPRs);
+
 function AddPR(){
+    let movement = document.getElementById('movement').value;
+    let weight = document.getElementById('weight').value;
+    if (movement && weight) {
+        let prList = document.getElementById('prList');
+        let listItem = document.createElement("li");
+        listItem.textContent = `${movement}: ${weight} kg`;
+        prList.appendChild(listItem);
+        savePR(movement, weight);
+        document.getElementById('movement').value = "";
+        document.getElementById('weight').value = "";
+    }
+}
+
+function savePR(){
+    let prs = JSON.parse(localStorage.getItem("prs")) || [];
+    prs.push({ moviment, weight });
+    localStorage.setItem("prs", JSON.stringify(prs));
+}
+
+function loadPRs(){
 
 }
 

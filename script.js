911bd826401f45f286b1299afcbf6d3363cdd503
Leonardo Trevisani <leo.hipolito64@hotmail.com>
2025-02-17 21:37:25 -0300

    some ajusts and more errors

--- script.js
+++ script.js
@@ -4,24 +4,31 @@ function AddPR(){
     let movement = document.getElementById('movement').value;
     let weight = document.getElementById('weight').value;
     if (movement && weight) {
-        let prList = document.getElementById('prList');
+        let prList = document.getElementById("prList");
         let listItem = document.createElement("li");
         listItem.textContent = `${movement}: ${weight} kg`;
         prList.appendChild(listItem);
         savePR(movement, weight);
-        document.getElementById('movement').value = "";
-        document.getElementById('weight').value = "";
+        document.getElementById("movement").value = "";
+        document.getElementById("weight").value = "";
     }
 }
 
 function savePR(){
     let prs = JSON.parse(localStorage.getItem("prs")) || [];
-    prs.push({ moviment, weight });
+    prs.push({ movement, weight });
     localStorage.setItem("prs", JSON.stringify(prs));
 }
 
 function loadPRs(){
-
+    let prs = JSON.parse(localStorage.getItem("prs")) || [];
+    let prList = document.getElementById("prList");
+    prList.innerHTML = ""
+    prs.forEach(pr => {
+        let listItem = document.createElement("li");
+        listItem.textContent = `${pr.movement}: ${pr.weight} kg`;
+        prList.appendChild(listItem)
+    });
 }
 
 function Convert(){
